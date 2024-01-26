c8b1791bfce7f03b0a3349d38c6abae7fb534be5
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-26 17:03:41 +0700

    update 2-reserve

--- 2-reserveSeq.js
+++ 2-reserveSeq.js
@@ -1,9 +1,13 @@
+// const reverseSeq = (n) => {
+//     const result = [];
+//     for (let i = n; i >= 1; i--) {
+//         result.push(i);
+//     }
+//   return result;
+// };
+
 const reverseSeq = (n) => {
-    const result = [];
-    for (let i = n; i >= 1; i--) {
-        result.push(i);
-    }
-  return result;
+    return Array(n).fill().map((_, i) => i + 1).reverse();
 };
 
 console.log(reverseSeq(5));
