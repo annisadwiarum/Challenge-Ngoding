56960501ce189f17d36680428f03ffeb01547b61
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-26 17:03:13 +0700

    2-reserve

--- 1-grow.js
+++ 1-grow.js
@@ -1,17 +1,18 @@
-function grow(x) {
-    let number = x[0];
-    for (let i = 1; i < x.length; i++) {
-        number += x[i];
-    }
-    return number;
-}
-
-console.log(grow([1, 2, 4]));
+// function grow(x) {
+//     let number = x[0];
+//     for (let i = 1; i < x.length; i++) {
+//         number += x[i];
+//     }
+//     return number;
+// }
 
 // reduce itu suatu fungsi untuk menghasilkan satu nilai dari banyak nilai di dalam sebuah array
 
 // function grow(x) {
-
+//     const result = x.reduce((acc, curr) => acc * curr, 1);
+//     return result;
 // }
 
-// console.log(grow([1, 2, 3, 4]));
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
+
+console.log(grow([1, 2, 5]));
